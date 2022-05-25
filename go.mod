80ade578c1d3399123c864294c3682102c0f511c
Sreesh Mallya <sreesh@keyvalue.systems>
2022-05-25 21:36:41 +0530

    Deleted other student files and changed structure

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sreesh-mallya/urlshort
+
+go 1.18
+
+require github.com/gophercises/urlshort v0.0.0-20190723121003-cc800dbaf411
---
+++ students/sreesh/urlshort/handler.go
@@ -0,0 +1,15 @@
+package urlshort
+
+import (
+	"net/http"
+)
+
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	//	TODO: Implement this...
+	return nil
+}
+
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	// TODO: Implement this...
+	return nil, nil
+}
